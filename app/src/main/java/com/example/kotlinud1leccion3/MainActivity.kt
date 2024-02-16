32a21201154f0e1e95f8f8a63331bb3ad7a91c0f
dalopea <d.lopez.william@gmail.com>
2024-02-16 17:08:08 +0100

    LEccion Imagen

--- app/src/main/java/com/example/kotlinud1leccion3/MainActivity.kt
+++ app/src/main/java/com/example/kotlinud1leccion3/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.kotlinud1leccion3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kotlinud1leccion3.ui.theme.KotlinUd1Leccion3Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +30,41 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingText(message = "Happy Birthday Kara!",from="From David", modifier=Modifier.padding(8.dp))
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingText(message:String,from:String,modifier:Modifier=Modifier){
+    Column(verticalArrangement = Arrangement.Center,
+        modifier=modifier) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+            )
+        Text(text = from,
+            fontSize = 36.sp,
+            modifier=Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End))
+    }
 }
+@Composable
+fun GreetingImage(message:String,from:String,modifier: Modifier=Modifier){
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     KotlinUd1Leccion3Theme {
-        Greeting("Android")
+        GreetingText(message = "Happy Birthday Kara!",from="From David")
     }
 }
