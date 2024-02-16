2fc06890466d8b18395ac0055ca879f08858515d
dalopea <d.lopez.william@gmail.com>
2024-02-16 16:26:02 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KotlinUd1Leccion3"
+include(":app")
